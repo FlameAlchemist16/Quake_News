c2db833be572074a5f8cbecc22e22c075f911da3
Mridul Khare <mridulkhr4@gmail.com>
2021-06-07 20:22:21 +0530

    Add files via upload

--- app/src/main/java/com/example/android/quakenews/news/NewsAdapter.java
+++ app/src/main/java/com/example/android/quakenews/news/NewsAdapter.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.quakenews.R;
@@ -15,7 +18,31 @@ import com.example.android.quakenews.earthquake.quake;
 
 import java.util.ArrayList;
 
-public class NewsAdapter extends ArrayList<quake> {
+public class NewsAdapter extends ArrayAdapter<quake> {
 
-    
+    public NewsAdapter(Activity context, ArrayList<quake> Quake)
+    {
+        super(context, 0, Quake);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View recycleView = convertView;
+        if(recycleView == null){
+            recycleView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_view_modifier, parent, false);
+        }
+        quake Quake = getItem(position);
+        TextView mag = (TextView) recycleView.findViewById(R.id.mag);
+        mag.setText(Quake.getMag());
+
+        TextView place = (TextView) recycleView.findViewById(R.id.place);
+        place.setText(Quake.getPlace());
+
+        TextView date = (TextView) recycleView.findViewById(R.id.day);
+        date.setText(Quake.getDate());
+
+        return recycleView;
+    }
 }
