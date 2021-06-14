7379e8a1ac390312d363b32526cab358ec8e9d7a
Mridul Khare <mridulkhr4@gmail.com>
2021-06-14 18:15:06 +0530

    Add files via upload

--- app/src/main/java/com/example/android/quakenews/news/NewsAdapter.java
+++ app/src/main/java/com/example/android/quakenews/news/NewsAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.example.android.quakenews.R;
 import com.example.android.quakenews.earthquake.Quake;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 
 public class NewsAdapter extends ArrayAdapter<Quake> {
 
+    private Integer magnitude;
     public NewsAdapter(Activity context, ArrayList<Quake> Quake)
     {
         super(context, 0, Quake);
@@ -31,6 +33,40 @@ public class NewsAdapter extends ArrayAdapter<Quake> {
                     R.layout.list_view_modifier, parent, false);
         }
         Quake Quake = getItem(position);
+        magnitude = Quake.getColor();
+        TextView textView = (TextView) recycleView.findViewById(R.id.mag_color);
+        switch (magnitude){
+            case 1:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag1));
+                break;
+            case 2:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag2));
+                break;
+            case 3:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag3));
+                break;
+            case 4:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag4));
+                break;
+            case 5:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag5));
+                break;
+            case 6:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag6));
+                break;
+            case 7:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag7));
+                break;
+            case 8:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag8));
+                break;
+            case 9:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag9));
+                break;
+            case 10:
+                textView.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.mag10));
+                break;
+        }
         TextView mag = (TextView) recycleView.findViewById(R.id.mag);
         mag.setText(Quake.getMag());
 
