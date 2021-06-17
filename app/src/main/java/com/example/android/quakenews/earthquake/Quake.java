02ff24aaf81042fb6e520d627c272bf9817f6021
Mridul Khare <mridulkhr4@gmail.com>
2021-06-17 19:21:51 +0530

    Add files via upload

--- app/src/main/java/com/example/android/quakenews/earthquake/Quake.java
+++ app/src/main/java/com/example/android/quakenews/earthquake/Quake.java
@@ -1,13 +1,14 @@
 package com.example.android.quakenews.earthquake;
 
 public class Quake {
-    private String mDir, mPlace, mMagnitude, mDate;
+    private String mDir, mPlace, mMagnitude, mDate, mUrl;
     private Integer mag;
-    public Quake(double magnitude, String place, String date)
+    public Quake(double magnitude, String place, String date, String url)
     {
         mag = (int) Math.floor(magnitude);
         mMagnitude = Double.toString(magnitude);
         mDate = date;
+        mUrl =url;
         int loc = place.indexOf("of");
         if(loc>=0&&loc<place.length()){
             mDir = place.substring(0, loc+2);
@@ -23,4 +24,5 @@ public class Quake {
     public String getDir() { return mDir;}
     public String getPlace() { return mPlace; }
     public Integer getColor() { return mag; }
+    public String getUrl() { return mUrl; }
 }
